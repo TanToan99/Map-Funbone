5094d6206a70cf39ba33a32c05ceb514e7501b77
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-09-23 22:49:53 +0700

    update

--- Assets/Script/SpawnPlayer.cs
+++ Assets/Script/SpawnPlayer.cs
@@ -5,22 +5,22 @@ using Photon.Pun;
 
 public class SpawnPlayer : MonoBehaviour
 {
-    public GameObject playerPrefab;
+  public GameObject playerPrefab;
 
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector2 randomPosition = new Vector2(Random.Range(minX,maxX), Random.Range(minY,maxY));
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
-    }
+  public float minX;
+  public float maxX;
+  public float minY;
+  public float maxY;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), -1);
+    PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
